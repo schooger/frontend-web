fe48c8fbd5122a581f293250f0110ae56b4329b6
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2024-12-02 21:21:08 +0100

    --

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core'
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <MantineProvider defaultColorScheme="light">
+    <App />
+  </MantineProvider>
 )
