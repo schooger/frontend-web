2f154efe3393ebd38e9321131c321c12bae00af7
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2024-12-13 12:22:07 +0100

    add page loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import '@mantine/core/styles.css';
@@ -11,9 +10,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider defaultColorScheme="light">
-        <App />
-      </MantineProvider>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <MantineProvider defaultColorScheme="light">
+      <App />
+    </MantineProvider>
+  </QueryClientProvider>
 )
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet, useMatchRoute } from '@tanstack/react-router'
 import AppHeader from "@layout/app-header.layout"
 import AppNavbar from "@layout/app-navbar.layout"
+import { Loader } from '@mantine/core';
 
 export const Route = createRootRoute({
   component: () => {
@@ -19,7 +20,9 @@ export const Route = createRootRoute({
         <AppNavbar />
 
         {isPending ? (
-          <div>Loading...</div>
+          <div className="flex justify-center flex-col items-center w-full py-8">
+            <Loader color="#444" size="md" type="dots" />
+          </div>
         ) : (
           <Outlet />
         )}
