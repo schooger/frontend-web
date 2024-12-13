fa2ab6302b4b74bd529ba3b7f1800caf89a24fac
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2024-12-13 14:53:04 +0100

    add dropdown menu to app-header

--- src/@layout/app-header.layout.tsx
+++ src/@layout/app-header.layout.tsx
@@ -1,5 +1,7 @@
 import { Select } from '@mantine/core';
-import { ChevronsUpDown, Languages } from 'lucide-react';
+import { Menu } from '@mantine/core';
+import { Link } from '@tanstack/react-router';
+import { ChevronsUpDown, Languages, LogOut, Settings, UserCircle } from 'lucide-react';
 
 export default function AppHeader() {
   //console.log('app-header')
@@ -53,12 +55,43 @@ export default function AppHeader() {
             />
           </div>
 
-          <div
-            className="w-[2.8rem] h-[2.8rem] bg-white bg-no-repeat bg-cover bg-center rounded-full overflow-hidden"
-            style={{ backgroundImage: "url('/image.jpg')" }}
-          ></div>
+          <Dropdown />
         </div>
       </div>
     </div>
   )
 }
+
+function Dropdown() {
+  return (
+    <Menu shadow="md" width={140} position="top-end">
+      <Menu.Target>
+        <button className='bg-transparent border-0 rounded-full'>
+          <div
+            className="w-[2.8rem] h-[2.8rem] bg-white bg-no-repeat bg-cover bg-center rounded-full overflow-hidden"
+            style={{ backgroundImage: "url('/image.jpg')" }}
+          ></div>
+        </button>
+      </Menu.Target>
+
+      <Menu.Dropdown className="font-bold">
+        <Menu.Item
+          leftSection={<UserCircle size={20} className="text-[#444]" />}
+        >
+          <Link to="/profile" className="text-[#444]">My Profile</Link>
+        </Menu.Item>
+        <Menu.Item
+          leftSection={<Settings size={20} className="text-[#444]" />}
+        >
+          <Link to="/settings" className="text-[#444]">Settings</Link>
+        </Menu.Item>
+        <Menu.Item
+          leftSection={<LogOut size={20} className="text-red-500" />}
+        >
+          <span className="text-red-500">Sign Out</span>
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
+
